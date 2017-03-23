f552547202177c5c7f40b29125c7b67c8743a9e3
asitaru <andreisitaru93@gmail.com>
2017-03-23 13:48:51 +0200

    url validation

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ let app=express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (req,res) => {
-    res.end('Working as intended');
+// app.get('/', (req,res) => {
+//     res.end('Working as intended');
+// });
+
+app.get('/new/*', (req,res) => {
+    if (validUrl.isUri(req.params[0])){
+        res.send('Looks like an URI');
+    } else {
+        res.send(JSON.stringify({error: "Wrong url format, make sure you have a valid protocol and real site."}, null, " "));
+    }
 });
 
 app.listen(app.get('port'), () => console.log('App is running on port', app.get('port')));
