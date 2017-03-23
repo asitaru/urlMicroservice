a5537d5fda73c9db919833cdf8756c5982c5ddb2
asitaru <andreisitaru93@gmail.com>
2017-03-23 14:24:03 +0200

    added basic db access

--- server.js
+++ server.js
@@ -7,13 +7,28 @@ let app=express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.get('/', (req,res) => {
-//     res.end('Working as intended');
-// });
+app.get('/:id', (req,res) => {
+    if(isNaN(req.params.id)) {
+        res.send(JSON.stringify({error: "This URL is not in DB"}, null, " "));
+    } else {
+        mongodb.connect(url, (err,db) => {
+            if(err) {
+                res.send(JSON.stringify({error: "Could not connect to DB, try again later!"}, null, " "));
+            } else {
+                res.send("Connection established to", url);
+            }
+            db.close();
+        });
+    }
+});
 
 app.get('/new/*', (req,res) => {
     if (validUrl.isUri(req.params[0])){
-        res.send('Looks like an URI');
+        let output = {
+            original_url: req.params[0],
+            short_url: "https://url-shorten-microservice.herokuapp.com/"
+        }
+        res.send(JSON.stringify(output, null, " "));
     } else {
         res.send(JSON.stringify({error: "Wrong url format, make sure you have a valid protocol and real site."}, null, " "));
     }
