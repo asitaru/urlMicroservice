4ec66020efc7a4c8a173b2f4813dc0a7238c65c8
asitaru <andreisitaru93@gmail.com>
2017-03-27 20:59:52 +0300

    refactored the entire project with promises

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.get('/', (req,res) => {
     res.sendFile('index.html');
 });
 
-let handleError = function(error) {
-    res.send(JSON.stringify({error: error}));
-}
-
-//Finding the URL in the database, if nothing found sends an error object to the response
+//Finds the URL in the database, if nothing found sends an error object to the response
 app.get('/:id', (req,res) => {
     if(isNaN(req.params.id)) {
         res.send(JSON.stringify({error: "This URL is not in DB"}, null, " "));
@@ -37,54 +33,36 @@ app.get('/:id', (req,res) => {
 
 app.get('/new/*', (req, res) => {
     if (validUrl.isUri(req.params[0])) {
-        mongodb.connect(url, (err, db) => {
-            if (err) handleError();
-            db.collection('url')
-                .find({
-                    original_url: req.params[0]
-                })
-                .toArray((err, result) => {
-                    if (err) handleError()
-                    else if (result.length) {
-                        result[0].short_url = "https://url-shorten-microservice.herokuapp.com/" +
-                            result[0].short_url;
-                        res.send(JSON.stringify({
-                            original_url: result[0].original_url,
-                            short_url: result[0].short_url
-                        }, null, " "));
-                        db.close();
-                    } else {
-                        var max;
-                        db.collection('url')
-                            .find()
-                            .sort({
-                                short_url: -1
-                            })
-                            .limit(1)
-                            .toArray((err, result) => {
-                                if (err) handleError()
-                                else{
-                                    console.log(result[0].short_url)
-                                    max = parseInt(result[0].short_url);
-                                    var obj = {
-                                        original_url: req.params[0],
-                                        short_url: max + 1
-                                    }
-                                };
-                                db.collection('url')
-                                    .insert(obj, (err, data) => {
-                                        if (err) handleError()
-                                        res.send(JSON.stringify({
-                                            original_url: obj.original_url,
-                                            short_url: "https://url-shorten-microservice.herokuapp.com/" + obj.short_url
-                                        }, null, " "));
-                                        db.close();
-                                    });
-                            })
-
-                    }
-                });
-        });
+        mongodb.connect(url)
+            .then(db => {
+                //Checks if the URL is already in the database
+                let resultObject = {};
+                db.collection('url').find({original_url: req.params[0]}).toArray()
+                    .then( result => {
+                        if(result.length) {
+                            resultObject.original_url = result[0].original_url;
+                            resultObject.short_url = `https://url-shorten-microservice.herokuapp.com/${result[0].short_url}`;
+                            res.send(JSON.stringify(resultObject, null, " "));
+                        } else {
+                            //If it's not, the URL will get inserted on the last position
+                            db.collection('url').find().sort({short_url: -1}).limit(1).toArray()
+                                .then( result => parseInt(result[0].short_url))
+                                .then( maxValue => resultObject = {original_url: req.params[0], short_url: maxValue + 1})
+                                .then( resultObject => {
+                                            db.collection('url').insert(resultObject)
+                                                .catch(error => res.send(JSON.stringify({error: error})))
+                                            return resultObject
+                                })
+                                .then(resultObject => {
+                                    res.send(JSON.stringify({
+                                        original_url: resultObject.original_url,
+                                        short_url:  `https://url-shorten-microservice.herokuapp.com/${resultObject.short_url}`
+                                    }, null, " "));
+                                })
+                        }
+                    })
+            })
+            .catch(error => res.end(JSON.stringify({error: error})))
     } else {
         res.send(JSON.stringify({
             error: "Wrong url format, make sure you have a valid protocol and real site."
