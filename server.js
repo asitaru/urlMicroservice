aa07aa382d9c50058105e722a10f724992013693
asitaru <andreisitaru93@gmail.com>
2017-03-28 21:25:56 +0300

    findUrl

--- server.js
+++ server.js
@@ -45,37 +45,47 @@ function getResponseObject(result) {
     }
 }
 
+function findUrl(query){
+    let promise = new Promise((resolve, reject) => {
+        db.collection.find(query).toArray().then(result => {
+            if(result.length) {
+                resolve(result);
+            } else {
+                reject("");
+            }
+        })
+    })
+    return promise;
+}
+
 app.get('/new/*', (req, res) => {
-    if (validUrl.isUri(req.params[0])) {
-        mongodb.connect(url)
-            .then(db => {
-                //Checks if the URL is already in the database
-                db.collection('url').find({original_url: req.params[0]}).toArray()
-                    .then( result => {
-                        if(result.length) {
-                            res.send(JSON.stringify(getResponseObject(result), null, " "));
-                        } else {
-                            //If it's not, the URL will get inserted on the last position
-                            db.collection('url').find().sort({short_url: -1}).limit(1).toArray()
-                                .then( result => parseInt(result[0].short_url))
-                                .then( maxValue => resultObject = {original_url: req.params[0], short_url: maxValue + 1})
-                                .then( resultObject => {
-                                            db.collection('url').insert(resultObject)
-                                                .catch(error => res.send(JSON.stringify({error: error})))
-                                            return resultObject
-                                })
-                                .then(resultObject => {
-                                    res.send(JSON.stringify(getResponseObject(resultObject), null, " "));
-                                })
-                        }
-                    })
-            })
-            .catch(error => res.end(JSON.stringify({error: error})))
-    } else {
+    if (!validUrl.isUri(req.params[0])) {
         res.send(JSON.stringify({
             error: "Wrong url format, make sure you have a valid protocol and real site."
         }, null, " "));
+        return
     }
+    mongodb.connect(url)
+        .then(db => {
+            //Checks if the URL is already in the database
+            findUrl({original_url: req.params[0]})
+                .then( result => res.send(JSON.stringify(getResponseObject(result), null, " ")))
+                .catch(reject => {
+                    db.collection('url').find().sort({short_url: -1}).limit(1).toArray()
+                        .then( result => parseInt(result[0].short_url))
+                        .then( maxValue => resultObject = {original_url: req.params[0], short_url: maxValue + 1})
+                        .then( resultObject => {
+                                    db.collection('url').insert(resultObject)
+                                        .catch(error => res.send(JSON.stringify({error: error})))
+                                    return resultObject
+                        })
+                        .then(resultObject => {
+                            res.send(JSON.stringify(getResponseObject(resultObject), null, " "));
+                        })
+                    })
+        })
+        .catch(error => res.end(JSON.stringify({error: error})))
+
 });
 
 app.listen(app.get('port'), () => console.log('App is running on port', app.get('port')));
